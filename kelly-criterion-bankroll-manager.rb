965e60fa56daa29efcc30c0bd07c39de6429fd9e
John Blank <veloblank@gmail.com>
2017-06-07 11:21:54 +0000

    push

--- kelly-criterion-bankroll-manager.rb
+++ kelly-criterion-bankroll-manager.rb
@@ -1,3 +1,5 @@
+require "colorize"
+
 print "Input Current Bankroll: "
 bankroll = gets.chomp.to_i
 puts "1u bet size = #{bankroll *0.010}"
@@ -10,10 +12,12 @@ print "******************************\n******************************\n"
 print "What is your Kelly Fraction (%): "
 kelly_fraction = gets.chomp.to_f / 100
 
-print "What are the odds of Side 1 win outcome (decimal): "
+puts "What are the odds of Side 1 win outcome (decimal): "
 odds_side1_win = gets.chomp.to_f
-print "What are the odds of Side 2 win outcome (decimal): "
+puts "Implied: #{(1/odds_side1_win).round(2)}"
+puts "What are the odds of Side 2 win outcome (decimal): "
 odds_side2_win = gets.chomp.to_f
+puts "Implied: #{(1/odds_side2_win).round(2)}"
 puts "\n \n"
 print "What is the handicapped probability of Side 1 win outcome (%): "
 prob_side1_win = gets.chomp.to_f / 100
@@ -24,13 +28,13 @@ puts "\n \n"
 percent_br_bet_prop_one = ((((odds_side1_win * prob_side1_win)-1))/(odds_side1_win-1))
 percent_br_bet_prop_two = ((((odds_side2_win * prob_side2_win)-1))/(odds_side2_win-1))
 
-sized_bet_prop_one = ((((odds_side1_win * prob_side1_win)-1))/(odds_side1_win-1)) * (bankroll) * kelly_fraction
-sized_bet_prop_two = ((((odds_side2_win * prob_side2_win)-1))/(odds_side2_win-1)) * (bankroll) * kelly_fraction
+sized_bet_prop_one = ((((odds_side1_win * prob_side1_win)-1))/(odds_side1_win-1)) * (bankroll) * (kelly_fraction)
+sized_bet_prop_two = ((((odds_side2_win * prob_side2_win)-1))/(odds_side2_win-1)) * (bankroll) * (kelly_fraction)
 
-full_kelly_prop_one = ((((odds_side1_win * prob_side1_win)-1))/(odds_side1_win-1)) * (bankroll)
-full_kelly_prop_two = ((((odds_side2_win * prob_side2_win)-1))/(odds_side2_win-1)) * (bankroll)
+full_one = ((((odds_side1_win * prob_side1_win)-1))/(odds_side1_win-1)) * (bankroll)
+full_two = ((((odds_side2_win * prob_side2_win)-1))/(odds_side2_win-1)) * (bankroll)
 
-if sized_bet_prop_one < 0 && sized_bet_prop_two < 0
+if sized_bet_prop_one <= 0 && sized_bet_prop_two <= 0
   puts "You should not make a bet on this game."
 elsif sized_bet_prop_one > sized_bet_prop_two
   puts "You should bet: $"+"#{sized_bet_prop_one.round(2)} at #{kelly_fraction*100}% Kelly on Side 1"
@@ -38,10 +42,13 @@ elsif
   puts "You should bet $"+"#{sized_bet_prop_two.round(2)} at #{kelly_fraction*100}% Kelly on Side 2"
 end
 
-puts "*****************Kelly Table*****************"
-puts "*                                           *"
-puts "*     At:   15%    30%    60%    75%    Full    *"
-puts "*                                           *"
-puts "* Side 1:  #{full_kelly_prop_one.round(2) * 0.15}   #{full_kelly_prop_one.round(2) * 0.30}   *"
-puts "*                                           *"
-puts "* Side 2:  #{full_kelly_prop_two.round(2) * 0.15}   #{full_kelly_prop_two.round(2) * 0.30}   *"
+puts "********************Kelly Table*******************"
+puts "*                                                *"
+puts "*     At:    15%    25%    50%    75%    Full    *"
+puts "*                                                *"
+puts "* Side 1:    #{full_one.round(2) * 0.15}    #{full_one.round(2) * 0.25}    #{full_one.round(2) * 0.50}    #{full_one.round(2) * 0.75}    #{full_one.round(2)} *"
+puts "*                                                *"
+puts "* Side 2:    #{full_two.round(2) * 0.15}    #{full_two.round(2) * 0.25}    #{full_two.round(2) * 0.50}    #{full_two.round(2) * 0.75}    #{full_two.round(2)} *"
+puts "*                                                *"
+puts "*                                                *"
+puts "**************************************************"
